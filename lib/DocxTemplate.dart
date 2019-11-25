26a6adaf59af388d362a2a085142256d4a131f3e
Pavel Snitko <brian57reg@gmail.com>
2019-11-25 22:17:48 +0300

    more flexible content building

--- lib/DocxTemplate.dart
+++
@@ -1,2 +0,0 @@
-export 'src/docs.dart' show DocxTemplate;
-export 'src/model.dart';
--- test/DocxTemplate_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:DocxTemplate/DocxTemplate.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    //expect(calculate(), 42);
-  });
-}
